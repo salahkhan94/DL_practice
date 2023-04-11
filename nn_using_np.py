04af18b59d375b272eb0d675d8ea526e82f2ba9c
Salahuddin Khan <salahuddin.khan.1994@gmail.com>
2023-04-11 13:38:03 +0530

    first commit

---
+++ nn_using_np.py
@@ -0,0 +1,159 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import os
+
+def load_data(data_dir):
+    X, y = [], []
+    for file_name in os.listdir(data_dir):
+        if file_name.endswith('.jpg'):
+            img = cv2.imread(os.path.join(data_dir, file_name))
+            img = cv2.resize(img, (64, 64))
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            img = np.array(img).flatten()
+            X.append(img)
+            if 'cat' in file_name:
+                y.append(1)
+            else:
+                y.append(0)
+    X = np.array(X)
+    y = np.array(y)
+    y = y.reshape(-1, 1)
+    return X, y
+
+data_dir_train = '/home/salahuddin/projects/nn_practice/datasets/catsanddogs/train/mix'
+X_train, y_train = load_data(data_dir_train)
+X_test, y_test = load_data('/home/salahuddin/projects/nn_practice/datasets/catsanddogs/test/mix')
+
+
+print(X_train.shape, y_train.shape)
+
+class NeuralNetwork:
+    def __init__(self, X, y, batch = 64, lr = 1e-3,  epochs = 50):
+        self.input = X 
+        self.target = y
+        self.batch = batch
+        self.epochs = epochs
+        self.lr = lr
+        
+        self.x = self.input[:self.batch] # batch input 
+        self.y = self.target[:self.batch] # batch target value
+        self.loss = []
+        self.acc = []
+        
+        self.init_weights()
+      
+    def init_weights(self):
+        self.W1 = np.random.randn(self.input.shape[1],256)
+        self.W2 = np.random.randn(self.W1.shape[1],128)
+        self.W3 = np.random.randn(self.W2.shape[1],self.y.shape[1])
+
+        self.b1 = np.random.randn(self.W1.shape[1],)
+        self.b2 = np.random.randn(self.W2.shape[1],)
+        self.b3 = np.random.randn(self.W3.shape[1],)
+
+    def ReLU(self, x):
+        return np.maximum(0,x)
+
+    def dReLU(self,x):
+        return 1 * (x > 0) 
+    
+    def softmax(self, z):
+        z = z - np.max(z, axis = 1).reshape(z.shape[0],1)
+        return np.exp(z) / np.sum(np.exp(z), axis = 1).reshape(z.shape[0],1)
+    
+    def shuffle(self):
+        idx = [i for i in range(self.input.shape[0])]
+        np.random.shuffle(idx)
+        self.input = self.input[idx]
+        self.target = self.target[idx]
+        
+    def feedforward(self):
+        assert self.x.shape[1] == self.W1.shape[0]
+        self.z1 = self.x.dot(self.W1) + self.b1
+        self.a1 = self.ReLU(self.z1)
+
+        assert self.a1.shape[1] == self.W2.shape[0]
+        self.z2 = self.a1.dot(self.W2) + self.b2
+        self.a2 = self.ReLU(self.z2)
+
+        assert self.a2.shape[1] == self.W3.shape[0]
+        self.z3 = self.a2.dot(self.W3) + self.b3
+        self.a3 = self.softmax(self.z3)
+        # print(self.z3)
+        self.error = self.a3 - self.y
+
+        
+    def backprop(self):
+        dcost = (1/self.batch)*self.error
+        
+        DW3 = np.dot(dcost.T,self.a2).T
+        DW2 = np.dot((np.dot((dcost),self.W3.T) * self.dReLU(self.z2)).T,self.a1).T
+        DW1 = np.dot((np.dot(np.dot((dcost),self.W3.T)*self.dReLU(self.z2),self.W2.T)*self.dReLU(self.z1)).T,self.x).T
+
+        db3 = np.sum(dcost,axis = 0)
+        db2 = np.sum(np.dot((dcost),self.W3.T) * self.dReLU(self.z2),axis = 0)
+        db1 = np.sum((np.dot(np.dot((dcost),self.W3.T)*self.dReLU(self.z2),self.W2.T)*self.dReLU(self.z1)),axis = 0)
+        
+        assert DW3.shape == self.W3.shape
+        assert DW2.shape == self.W2.shape
+        assert DW1.shape == self.W1.shape
+        
+        assert db3.shape == self.b3.shape
+        assert db2.shape == self.b2.shape
+        assert db1.shape == self.b1.shape 
+        
+        self.W3 = self.W3 - self.lr * DW3
+        self.W2 = self.W2 - self.lr * DW2
+        self.W1 = self.W1 - self.lr * DW1
+        
+        self.b3 = self.b3 - self.lr * db3
+        self.b2 = self.b2 - self.lr * db2
+        self.b1 = self.b1 - self.lr * db1
+
+
+    def train(self):
+        for epoch in range(self.epochs):
+            l = 0
+            acc = 0
+            self.shuffle()
+            
+            for batch in range(self.input.shape[0]//self.batch-1):
+                start = batch*self.batch
+                end = (batch+1)*self.batch
+                self.x = self.input[start:end]
+                self.y = self.target[start:end]
+                print(self.error, 'testing')
+                self.feedforward()
+                self.backprop()
+                l+=np.mean(self.error**2)
+                acc+= np.count_nonzero(np.argmax(self.a3,axis=1) == np.argmax(self.y,axis=1)) / self.batch
+                
+            self.loss.append(l/(self.input.shape[0]//self.batch))
+            self.acc.append(acc*100/(self.input.shape[0]//self.batch))
+            
+    def plot(self):
+        plt.figure(dpi = 125)
+        plt.plot(self.loss)
+        plt.xlabel("Epochs")
+        plt.ylabel("Loss")
+    
+    def acc_plot(self):
+        plt.figure(dpi = 125)
+        plt.plot(self.acc)
+        plt.xlabel("Epochs")
+        plt.ylabel("Accuracy")
+        
+    def test(self,xtest,ytest):
+        self.x = xtest
+        self.y = ytest
+        self.feedforward()
+        acc = np.count_nonzero(np.argmax(self.a3,axis=1) == np.argmax(self.y,axis=1)) / self.x.shape[0]
+        print("Accuracy:", 100 * acc, "%")
+    
+NN = NeuralNetwork(X_train, y_train)
+ 
+NN.train()
+NN.plot()
+NN.test(X_test,y_test)
+NN.test(X_train,y_train)
---
+++ np_simple_nn.py
@@ -0,0 +1,132 @@
+import numpy as np
+import os
+import cv2
+
+def load_data(data_dir):
+    X, y = [], []
+    for file_name in os.listdir(data_dir):
+        if file_name.endswith('.jpg'):
+            img = cv2.imread(os.path.join(data_dir, file_name))
+            img = cv2.resize(img, (64, 64))
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            img = np.array(img).flatten()
+            X.append(img)
+            if 'cat' in file_name:
+                y.append(1)
+            else:
+                y.append(0)
+    y = np.array(y)
+    y = y.reshape(-1, 1)
+    X = np.array(X)
+    return X, y
+
+data_dir = '/home/salahuddin/projects/nn_practice/datasets/catsanddogs/train/mix'
+X, y = load_data(data_dir)
+# np.squeeze(X)
+print(X.shape, y.shape)
+# split data into train, validation and test sets
+n_samples = len(X)
+n_train = int(1 * n_samples)
+n_val = int(0 * n_samples)
+n_test = n_samples - n_train - n_val
+
+indices = np.random.permutation(n_samples)
+X_train = X[indices[:n_train]]
+y_train = y[indices[:n_train]]
+X_val = X[indices[n_train:n_train+n_val]]
+y_val = y[indices[n_train:n_train+n_val]]
+X_test = X[indices[n_train+n_val:]]
+y_test = y[indices[n_train+n_val:]]
+
+# Define the sigmoid activation function
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+
+# Define the derivative of the sigmoid function
+def sigmoid_prime(z):
+    return sigmoid(z) * (1 - sigmoid(z))
+
+# Define the forward pass function
+def forward_pass(X, W1, b1, W2, b2):
+    # Compute the activation of the hidden layer
+    z1 = np.dot(X, W1) + b1
+    a1 = sigmoid(z1)
+
+    # Compute the activation of the output layer
+    z2 = np.dot(a1, W2) + b2
+    a2 = sigmoid(z2)
+
+    # Return the activations of the hidden and output layers
+    return a1, a2
+
+# Define the backward pass function
+def backward_pass(X, y, a1, a2, W1, b1, W2, b2, learning_rate):
+    # Compute the error in the output layer
+    delta2 = (a2 - y) * sigmoid_prime(a2)
+
+    # Compute the error in the hidden layer
+    delta1 = np.dot(delta2, W2.T) * sigmoid_prime(a1)
+
+    # Compute the gradients of the weights and biases
+    dW2 = np.dot(a1.T, delta2)
+    db2 = np.sum(delta2, axis=0, keepdims=True)
+    dW1 = np.dot(X.T, delta1)
+    db1 = np.sum(delta1, axis=0, keepdims=True)
+
+    # Update the weights and biases
+    W2 -= learning_rate * dW2
+    b2 -= learning_rate * db2
+    W1 -= learning_rate * dW1
+    b1 -= learning_rate * db1
+
+    # Return the updated weights and biases
+    return W1, b1, W2, b2
+
+
+a1_ = 0
+a2_ = 0
+# Define the training loop
+def train(X_train, y_train, n_epochs, learning_rate):
+    # Initialize the weights and biases
+    input_size = X_train.shape[1]
+    hidden_size = 16
+    output_size = 1
+    W1 = np.random.randn(input_size, hidden_size) * 0.01
+    b1 = np.zeros((1, hidden_size))
+    W2 = np.random.randn(hidden_size, output_size) * 0.01
+    b2 = np.zeros((1, output_size))
+    # Train the model fo1r the specified number of epochs
+    bs = 0
+    for i in range(n_epochs):
+        # Perform a forward pass on the training data
+        a1, a2 = forward_pass(X_train, W1, b1, W2, b2)
+        if (not bs):
+            a1_ = a1
+            a2_ = a2
+            bs = 1
+        # Compute the loss
+        loss = np.mean((a2 - y_train) ** 2)
+
+        # Print the loss every 100 epochs
+        if i % 100 == 0:
+            print('Epoch', i, 'loss:', loss)
+
+        # Perform a backward pass to update the weights and biases
+        W1, b1, W2, b2 = backward_pass(X_train, y_train, a1, a2, W1, b1, W2, b2, learning_rate)
+
+    # Return the trained weights and biases
+    return W1, b1, W2, b2
+
+
+def plot_error(a1, a2, y_train) :
+    loss = np.mean((a2 - y_train) ** 2)
+    print("initial loss")
+    print(loss)
+
+W1, b1, W2, b2 = train(X_train, y_train, n_epochs=500, learning_rate=1e-3)
+plot_error(a1_, a2_, y_train)
+# Train the model
+a1, a2 = forward_pass(X_train, W1, b1, W2, b2)
+loss = np.mean((a2 - y_train) ** 2)
+print("Loss after traning")
+print(loss)
