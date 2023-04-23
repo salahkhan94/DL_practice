861bf507464af406b5866b42079e09637f80fea9
Salahuddin Khan <salahuddin.khan.1994@gmail.com>
2023-04-23 19:05:47 +0530

    update

---
+++ catdetector.py
@@ -0,0 +1,95 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+class plot_error_surfaces(object):
+    
+    # Constructor
+    def __init__(self, w_range, b_range, X, Y, n_samples = 30, go = True):
+        W = np.linspace(-w_range, w_range, n_samples)
+        B = np.linspace(-b_range, b_range, n_samples)
+        w, b = np.meshgrid(W, B)    
+        Z = np.zeros((30, 30))
+        count1 = 0
+        self.y = Y.numpy()
+        self.x = X.numpy()
+        for w1, b1 in zip(w, b):
+            count2 = 0
+            for w2, b2 in zip(w1, b1):
+                Z[count1, count2] = np.mean((self.y - w2 * self.x + b2) ** 2)
+                count2 += 1
+            count1 += 1
+        self.Z = Z
+        self.w = w
+        self.b = b
+        self.W = []
+        self.B = []
+        self.LOSS = []
+        self.n = 0
+        if go == True:
+            plt.figure()
+            plt.figure(figsize = (7.5, 5))
+            plt.axes(projection = '3d').plot_surface(self.w, self.b, self.Z, rstride = 1, cstride = 1, cmap = 'viridis', edgecolor = 'none')
+            plt.title('Loss Surface')
+            plt.xlabel('w')
+            plt.ylabel('b')
+            plt.show()
+            plt.figure()
+            plt.title('Loss Surface Contour')
+            plt.xlabel('w')
+            plt.ylabel('b')
+            plt.contour(self.w, self.b, self.Z)
+            plt.show()
+            
+     # Setter
+    def set_para_loss(self, W, B, loss):
+        self.n = self.n + 1
+        self.W.append(W)
+        self.B.append(B)
+        self.LOSS.append(loss)
+    
+    # Plot diagram
+    def final_plot(self): 
+        ax = plt.axes(projection = '3d')
+        ax.plot_wireframe(self.w, self.b, self.Z)
+        ax.scatter(self.W, self.B, self.LOSS, c = 'r', marker = 'x', s = 200, alpha = 1)
+        plt.figure()
+        plt.contour(self.w, self.b, self.Z)
+        plt.scatter(self.W, self.B, c = 'r', marker = 'x')
+        plt.xlabel('w')
+        plt.ylabel('b')
+        plt.show()
+    
+    # Plot diagram
+    def plot_ps(self):
+        plt.subplot(121)
+        plt.ylim()
+        plt.plot(self.x, self.y, 'ro', label = "training points")
+        plt.plot(self.x, self.W[-1] * self.x + self.B[-1], label = "estimated line")
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.title('Data Space Iteration: '+ str(self.n))
+        plt.subplot(122)
+        plt.contour(self.w, self.b, self.Z)
+        plt.scatter(self.W, self.B, c = 'r', marker = 'x')
+        plt.title('Loss Surface Contour')
+        plt.xlabel('w')
+        plt.ylabel('b')
+        plt.show()
+
+
+
+import torch
+torch.manual_seed(1)
+
+
+X = torch.arange(-3, 3, 0.1).view(-1, 1)
+f = 1 * X - 1
+Y = f + 0.1 * torch.randn(X.size())
+
+plt.plot(X.numpy(), Y.numpy(), 'rx', label = 'y')
+plt.plot(X.numpy(), f.numpy(), label = 'f')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.show()
