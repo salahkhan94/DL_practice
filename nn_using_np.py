4e3c8ecffaa0ad17661cfb93690f510389092fa3
Salahuddin Khan <salahuddin.khan.1994@gmail.com>
2023-05-03 17:12:44 +0530

    Update

--- nn_using_np.py
+++ nn_using_np.py
@@ -1,7 +1,12 @@
+from torch.nn import Module, Conv2d, Linear, AdaptiveAvgPool2d
+from torch.nn.functional import relu
 import numpy as np
-import matplotlib.pyplot as plt
 import cv2
 import os
+import torch
+import torch.nn as nn
+
+data_dir = '/home/salahuddin/projects/Deeplearning_Practice/mix/'
 
 def load_data(data_dir):
     X, y = [], []
@@ -9,151 +14,81 @@ def load_data(data_dir):
         if file_name.endswith('.jpg'):
             img = cv2.imread(os.path.join(data_dir, file_name))
             img = cv2.resize(img, (64, 64))
-            img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-            img = np.array(img).flatten()
+            img = torch.tensor(img).permute(2, 0, 1) / 255.0
             X.append(img)
             if 'cat' in file_name:
                 y.append(1)
             else:
                 y.append(0)
-    X = np.array(X)
-    print(X.shape)
-    y = np.array(y)
+    X = torch.stack(X)
+    y = torch.tensor(y)
     y = y.reshape(-1, 1)
+    # print('test', X.shape)
+    y1 = torch.tensor(1)
+    y = torch.add(y, y1)
+    # print(y.shape)
     return X, y
 
-data_dir_train = '/home/salahuddin/projects/nn_practice/datasets/catsanddogs/train/mix'
-X_train, y_gt = load_data(data_dir_train)
+class Network(Module):
+    def __init__(self):
+        super(Network, self).__init__()
+        self.conv1 = Conv2d(in_channels=3, out_channels=64, kernel_size=5)
+        self.conv2 = Conv2d(in_channels=64, out_channels=64, kernel_size=5, stride=1, padding=4) 
+        self.conv3 = Conv2d(in_channels=64, out_channels=256, kernel_size=5) 
+        self.avgPooling = AdaptiveAvgPool2d(output_size=(256, 256))
+        self.fc1 = Linear(in_features=256, out_features=128)
+        self.fc2 = Linear(in_features=128, out_features=64)
+        self.out = Linear(in_features=64, out_features=2)
 
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.avgPooling(x)
+        x = self.conv2(x)
+        x = relu(x)
+        x = self.avgPooling(x)
+        x = self.conv3(x)
+        x = relu(x)
 
-print(X_train.shape, y_gt.shape)
+        x = self.avgPooling(x)    
+        x = self.fc1(x)
+        x = relu(x)
+        x = self.fc2(x)
+        x = relu(x)
+        x = self.out(x)
+        return x
 
-class NeuralNetwork:
-    def __init__(self, X, y, batch = 64, lr = 1e-3,  epochs = 50):
-        self.input = X 
-        self.target = y
-        self.batch = batch
-        self.epochs = epochs
-        self.lr = lr
-        
-        self.x = self.input[:self.batch] # batch input 
-        self.y = self.target[:self.batch] # batch target value
-        self.loss = []
-        self.acc = []
-        
-        self.init_weights()
-      
-    def init_weights(self):
-        self.W1 = np.random.randn(self.input.shape[1],256)
-        self.W2 = np.random.randn(self.W1.shape[1],128)
-        self.W3 = np.random.randn(self.W2.shape[1],self.y.shape[1])
+model = Network()
+learning_rate = 0.1
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+criterion = torch.nn.CrossEntropyLoss()
 
-        self.b1 = np.random.randn(self.W1.shape[1],)
-        self.b2 = np.random.randn(self.W2.shape[1],)
-        self.b3 = np.random.randn(self.W3.shape[1],)
+train_loader = load_data(data_dir=data_dir)
+N_train = len(train_loader[0])
+n_epochs = 3
 
-    def ReLU(self, x):
-        return np.maximum(0,x)
-
-    def dReLU(self,x):
-        return 1 * (x > 0) 
-    
-    def softmax(self, z):
-        z = z - np.max(z, axis = 1).reshape(z.shape[0],1)
-        return np.exp(z) / np.sum(np.exp(z), axis = 1).reshape(z.shape[0],1)
-    
-    def shuffle(self):
-        idx = [i for i in range(self.input.shape[0])]
-        np.random.shuffle(idx)
-        self.input = self.input[idx]
-        self.target = self.target[idx]
-        
-    def feedforward(self):
-        assert self.x.shape[1] == self.W1.shape[0]
-        self.z1 = self.x.dot(self.W1) + self.b1
-        self.a1 = self.ReLU(self.z1)
-
-        assert self.a1.shape[1] == self.W2.shape[0]
-        self.z2 = self.a1.dot(self.W2) + self.b2
-        self.a2 = self.ReLU(self.z2)
-
-        assert self.a2.shape[1] == self.W3.shape[0]
-        self.z3 = self.a2.dot(self.W3) + self.b3
-        self.a3 = self.softmax(self.z3)
-        # print(self.z3)
-        self.error = self.a3 - self.y
-
-        
-    def backprop(self):
-        dcost = (1/self.batch)*self.error
-        
-        DW3 = np.dot(dcost.T,self.a2).T
-        DW2 = np.dot((np.dot((dcost),self.W3.T) * self.dReLU(self.z2)).T,self.a1).T
-        DW1 = np.dot((np.dot(np.dot((dcost),self.W3.T)*self.dReLU(self.z2),self.W2.T)*self.dReLU(self.z1)).T,self.x).T
-
-        db3 = np.sum(dcost,axis = 0)
-        db2 = np.sum(np.dot((dcost),self.W3.T) * self.dReLU(self.z2),axis = 0)
-        db1 = np.sum((np.dot(np.dot((dcost),self.W3.T)*self.dReLU(self.z2),self.W2.T)*self.dReLU(self.z1)),axis = 0)
-        
-        assert DW3.shape == self.W3.shape
-        assert DW2.shape == self.W2.shape
-        assert DW1.shape == self.W1.shape
-        
-        assert db3.shape == self.b3.shape
-        assert db2.shape == self.b2.shape
-        assert db1.shape == self.b1.shape 
-        
-        self.W3 = self.W3 - self.lr * DW3
-        self.W2 = self.W2 - self.lr * DW2
-        self.W1 = self.W1 - self.lr * DW1
-        
-        self.b3 = self.b3 - self.lr * db3
-        self.b2 = self.b2 - self.lr * db2
-        self.b1 = self.b1 - self.lr * db1
-
-
-    def train(self):
-        for epoch in range(self.epochs):
-            l = 0
-            acc = 0
-            self.shuffle()
+def train_model(n_epochs):
+    for epoch in range(n_epochs):
+        COST = 0
+        # print(train_loader[0][1].shape)
+        for i in range(N_train):
+            X = train_loader[0][i].unsqueeze(0)
+            y = train_loader[1][i]
+            optimizer.zero_grad()
+            # print(X.shape)
+            z = model.forward(X)
+            # print(y.shape)
+            # print(z.shape)
+            z = z.view(1, -1)
             
-            for batch in range(self.input.shape[0]//self.batch-1):
-                start = batch*self.batch
-                end = (batch+1)*self.batch
-                self.x = self.input[start:end]
-                self.y = self.target[start:end]
-                print(self.error, 'testing')
-                self.feedforward()
-                self.backprop()
-                l+=np.mean(self.error**2)
-                acc+= np.count_nonzero(np.argmax(self.a3,axis=1) == np.argmax(self.y,axis=1)) / self.batch
-                
-            self.loss.append(l/(self.input.shape[0]//self.batch))
-            self.acc.append(acc*100/(self.input.shape[0]//self.batch))
-            
-    def plot(self):
-        plt.figure(dpi = 125)
-        plt.plot(self.loss)
-        plt.xlabel("Epochs")
-        plt.ylabel("Loss")
-    
-    def acc_plot(self):
-        plt.figure(dpi = 125)
-        plt.plot(self.acc)
-        plt.xlabel("Epochs")
-        plt.ylabel("Accuracy")
-        
-    def test(self,xtest,ytest):
-        self.x = xtest
-        self.y = ytest
-        self.feedforward()
-        acc = np.count_nonzero(np.argmax(self.a3,axis=1) == np.argmax(self.y,axis=1)) / self.x.shape[0]
-        print("Accuracy:", 100 * acc, "%")
-    
-NN = NeuralNetwork(X_train, y_gt)
- 
-# NN.train()
-# NN.plot()
-# NN.test(X_test,y_test)
-# NN.test(X_train,y_train)
+            criterion = nn.CrossEntropyLoss()
+            loss = criterion(z, y)
+            loss.backward()
+            optimizer.step()
+            COST += loss.item()
+        print(COST)
+        # cost_list.append(COST / N_train)
+        # correct = 0
+        # N_val = len(val_loader[0])
+        # # perform a prediction on the validation data
+        # with
+train_model(n_epochs)
